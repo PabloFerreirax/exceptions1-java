e67ee9f160f0148332dcdc129f4f6928908f44ce
PabloFerreirax <pablo.estudoss@gmail.com>
2020-07-30 23:45:01 -0300

    Solução ruim

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,12 +5,14 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
+import javax.sound.sampled.ReverbType;
+
 import model.entities.Reservation;
 
 public class Program {
 // quando o metodo pode dar erro tem que tratar ou colocar throws com o nome da excecao pois pode dar erro e assim propaga ela
 	public static void main(String[] args) throws ParseException {
-		
+
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
@@ -21,14 +23,29 @@ public class Program {
 		System.out.print("Check-Out (dd/MM/yyyy): ");
 		Date checkOut = sdf.parse(sc.next());
 		// after mostra se uma data é depois da outra
-		if(!checkOut.after(checkIn)) {
+		if (!checkOut.after(checkIn)) {
 			System.out.println("Erro data de check out depois de check in");
-		}
+		} 
 		else {
 			Reservation reserv = new Reservation(room, checkIn, checkOut);
 			System.out.println("Reservation: " + reserv);
+
+			System.out.println("Update na reserva: ");
+			System.out.print("Check-in (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Check-Out (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+			
+			// aqui atribuimos este update caso der erro a uma string error
+			String error = reserv.updateDays(checkIn, checkOut);
+			if(error != null) {
+				System.out.println("erro: " + error);
+			}
+			else {
+				System.out.println("Reservation: " + reserv);
+			}
 		}
-		
+
 		sc.close();
 	}
 
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -5,12 +5,12 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
-	
+
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
 		super();
 		this.roomNumber = roomNumber;
@@ -33,30 +33,36 @@ public class Reservation {
 	public Date getCheckOut() {
 		return checkOut;
 	}
-	
+
 	// duração em dias da estadia
 	public long duration() {
 		// macete para pegar a distancia entre duas datas em milesegundos
 		long diff = checkOut.getTime() - checkIn.getTime();
-		//transformando em dias esse resultado a cima
+		// transformando em dias esse resultado a cima
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	
-	public void updateDays(Date checkIn, Date checkOut) {
+
+	public String updateDays(Date checkIn, Date checkOut) {
+
+		// Serve para não deixar atualizar datas que não sejam futuras
+		Date now = new Date();
+		if (checkIn.before(now) && checkOut.before(now)) {
+			return "Erro não pode aceitar tem que ser datas futuras";
+		}
+		if (!checkOut.after(checkIn)) {
+			return "Erro data de check out depois de check in";
+		}
+
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		// não deu erro
+		return null;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Room"
-				+ roomNumber
-				+ ", Check-in"
-				+ sdf.format(checkIn)
-				+", Check-out"
-				+ sdf.format(checkOut)
-				+ duration()
-				+ " Nights";
+		return "Room " + roomNumber + ", Check-in " + sdf.format(checkIn) + ", Check-out " + sdf.format(checkOut)
+				+ duration() + " Nights";
 	}
-	
+
 }
